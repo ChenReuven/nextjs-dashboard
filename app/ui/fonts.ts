4402496e21eb7397c76fe33e687fae6caf885c70
Chen Reuven <chen.reuven@cyberark.com>
2023-12-06 09:51:47 +0200

    feat: add pages and layouts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, IBM_Plex_Mono } from 'next/font/google';
+
+export  const inter = Inter({subsets: ['latin'], weight: "400"});
+export  const ibmFlexMono = IBM_Plex_Mono({subsets: ['latin'], weight: "700"});
